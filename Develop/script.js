306ce8ed5ce367ebe5b8d83b3455aa6b52a8951f
Nomin Myagmarsuren <nominmyagmarsuren@gmail.com>
2023-08-04 18:07:31 -0600

    added html code, time blocks

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,17 @@
+// GIVEN I am using a daily planner to create a schedule
+// WHEN I open the planner
+// THEN the current day is displayed at the top of the calendar
+// WHEN I scroll down
+// THEN I am presented with time blocks for standard business hours of 9am to 5pm
+// WHEN I view the time blocks for that day
+// THEN each time block is color-coded to indicate whether it is in the past, present, or future
+// WHEN I click into a time block
+// THEN I can enter an event
+// WHEN I click the save button for that time block
+// THEN the text for that event is saved in local storage
+// WHEN I refresh the page
+// THEN the saved events persist
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
@@ -21,3 +35,5 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+
+var past = document
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Work Day Scheduler Starter Code
+
+## Description
+
+## Installation
+n/a
+
+## Usage
+
+
+## Credit
+Original code cloned from starter repo "crispy-octo-meme" from user Xandromus. 
+
+## License
+Please refer to "LICENSE" in the repository.
